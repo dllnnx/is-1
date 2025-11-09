1ad8ca5333835494c748cfd4bcf4a82ffd2c99de
dllnnx <alenadenisova528@gmail.com>
2025-11-09 03:18:16 +0300

    update baseurl and dockerfile

--- frontend/app/baseApi.ts
+++ frontend/app/baseApi.ts
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:8080/',
+    baseUrl: 'http://89.169.150.230:8080/',
 });
 
 export const api = createApi({
